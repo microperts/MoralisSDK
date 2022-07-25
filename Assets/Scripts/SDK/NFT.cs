a0bf5864957f74584c354b0d1ac03a85f9f9945d
Wajahat Developer <wajahatdeveloperqs@gmail.com>
2022-07-25 16:18:41 +0500

    Query NFTs

--- Assets/Scripts/SDK/NFT.cs
+++ Assets/Scripts/SDK/NFT.cs
@@ -27,10 +27,50 @@ namespace MoralisSDK
         }
         #endregion
 
+        public enum NftFilter
+        {
+            Name,
+            Description,
+            Attributes,
+            Global
+        }
+
         public async Task<int> GetUniqueCount()
         {
             NftOwnerCollection polygonNFTs = await Moralis.Web3Api.Account.GetNFTs(Common.Instance.moralisUser.ethAddress.ToLower(), Moralis.CurrentChain.EnumValue);
             return polygonNFTs.Result.Count;
+        }
+
+        public async Task<NftMetadataCollection> GetNFT(string query, NftFilter filter)
+        {
+            string filterString = "global";
+
+            switch (filter)
+            {
+                case NftFilter.Name:
+                    filterString = "name";
+                    break;
+                case NftFilter.Description:
+                    filterString = "description";
+                    break;
+                case NftFilter.Attributes:
+                    filterString = "attributes";
+                    break;
+                case NftFilter.Global:
+                    filterString = "global";
+                    break;
+                default:
+                    break;
+            }
+
+            NftMetadataCollection nfts = await Moralis.Web3Api.Token.SearchNFTs(q: query, Moralis.CurrentChain.EnumValue, filter: filterString);
+            return nfts;
+        }
+
+        public async Task<NftOwnerCollection> GetAllNFTs()
+        {
+            NftOwnerCollection nfts = await Moralis.Web3Api.Account.GetNFTs(Common.Instance.moralisUser.ethAddress.ToLower(), Moralis.CurrentChain.EnumValue);
+            return nfts;
         }
     }
 }
